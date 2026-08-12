8e1627fd08e23355b774948ae15b9ae65c2c6b45
Anson Terrell <anson.terrell@zurn.com>
2026-08-12 16:52:15 -0500

    initial commit
    - initial commit of repo

---
+++ gbc-tracker/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace gbc_tracker;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
